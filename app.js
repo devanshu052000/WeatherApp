392a3249da8b7a03a4310e80fefa8db5173aaf26
TheObjectOrientedGuy <73549716+TheObjectOrientedGuy@users.noreply.github.com>
2022-09-20 22:04:11 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res){
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/",function(req, res){
+  const query = req.body.cityName;
+  const apiKey = "725bc8e7d37f50a3afa16d73d8664602";
+  const unit = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
+  https.get(url, function (response){
+    console.log(response);
+    response.on("data",function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const desc = weatherData.weather[0].description;
+      const icon = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png";
+      res.write("<h1>The temperature in " + query + " is:" + temp + " degrees celcius.</h1>")
+      res.write("<img src="+icon+">")
+      res.send()
+    })
+  })
+
+})
+
+app.listen(3000, function (){
+  console.log("Server is running on port 3000.");
+});
